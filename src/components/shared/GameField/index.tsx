6f147d858a09ed3e7ff3a6493725a4b356e4c6ff
artistotless <neuralink7232050@gmail.com>
2025-04-05 22:52:25 +0300

    resolved changes conflict

--- src/components/shared/GameField/index.tsx
+++ src/components/shared/GameField/index.tsx
@@ -1,23 +1,16 @@
-import { useGame } from "src/contexts/GameContext";
-import { Player } from "../../ui/Player";
 import PlayerCards from "../PlayerCards";
 import Table from "../Table";
 import styles from "./gamefield.module.scss";
-import { IFoolPlayer } from "src/types";
-import { useUser } from "src/contexts/UserContext";
-import userImg from "src/assets/img/user.svg";
 import PassButton from "src/components/ui/PassButton";
 import TopPanel from "../TopPanel";
 
 const GameField = () => {
-   const { state, personalState } = useGame();
-   const { user } = useUser();
-   
+
    return (
       <div className={styles.field}>
          <TopPanel />
          <Table />
-         <PlayerCards cards={personalState.cardsInHand} />
+         <PlayerCards/>
          <PassButton />
       </div>
    );
